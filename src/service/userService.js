4e0a4fd7c40edd16d73140536a9a54f5df37a8e0
Duc PC <fishjiro@gmail.com>
2024-09-21 12:49:48 +0700

    fix ORMpath update CR(U)D to ORM

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,7 @@
 import mysql from 'mysql2/promise';
 import Bluebird from 'bluebird';
 import bcrypt from 'bcryptjs';
+import db from '../models/index';
 const salt = bcrypt.genSaltSync(10);
 
 const hashPassword = (userPassword) => {
@@ -9,44 +10,43 @@ const hashPassword = (userPassword) => {
 }
 
 const createUser = async (email, password) => {
-    const conn = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'test', Promise: Bluebird
-    })
     try {
         let hashedPassword = hashPassword(password);
-        const [rows, fields] = await conn.execute('insert Users(email,password) values (?,?)', [email, hashedPassword]);
-
+        // const [rows, fields] = await conn.execute('insert Users(email,password) values (?,?)', [email, hashedPassword]);
+        await db.User.create({
+            email: email,
+            password: hashedPassword,
+            username: 'aa'
+        })
     } catch (err) {
         console.log(err);
     }
 }
 
 const readUser = async () => {
-    const conn = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'test', Promise: Bluebird
-    })
+    // const conn = await mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     database: 'test', Promise: Bluebird
+    // })
     let user = [];
     try {
-        const [rows, fields] = await conn.execute("select * from Users");
-        return rows;
+        // const [rows, fields] = await conn.execute("select * from Users");
+        // return rows;
+        user = await db.User.findAll();
+        return user;
     } catch (err) {
         console.log(err);
     }
 }
 
 const deleteUser = async (id) => {
-    const conn = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'test', Promise: Bluebird
-    })
     try {
-        const [rows, fields] = await conn.execute("delete from Users where id=?", [id]);
-        return rows;
+        // const [rows, fields] = await conn.execute("delete from Users where id=?", [id]);
+        // return rows;
+        await db.User.destroy({
+            where: { id: id }
+        })
     } catch (err) {
     }
 }
